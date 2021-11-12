4206a4b40f40a9276fc3ab80d6d327c62951975e
Nazar <nazhrabarchuk@gmail.com>
2021-11-12 18:31:35 +0200

    +1 positive test for check return type

--- test/test_possitive.js
+++ test/test_possitive.js
@@ -2,15 +2,17 @@ import { assert, expect } from "chai"
 import {calculator} from "../app.js"
 
 describe("Fields of positive tests for calculator function", function() {
+        it('calculator function should return type number', ()=>{
+            let result = calculator(1,1,'+');
+            assert.typeOf(result, 'number');
+        });
         it('I expect 8 when adding 5 + 3', ()=>{
             expect(calculator(5,3,'+')).to.eq(8);
         });
-
         it('I expect 1.5 when divide 3 / 2', ()=>{
             expect(calculator(3,2,'/')).to.eq(1.5);
         });
         it('I expect -5 when subtraction 5 - 10', ()=>{
             expect(calculator(5,10,'-')).to.eq(-5);
         });
-   
 })
