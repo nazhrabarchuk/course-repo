8df58f2c0125ad4475130d5de8375a241fa2bd07
Nazar <nazhrabarchuk@gmail.com>
2021-11-17 11:05:58 +0200

    010 mocha_chai hw fixed

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 function calculator(num1, num2, operation) {
     const operations = ['+', '-', '/', '*'];
+   
     if(!operations.includes(operation)){
-        throw Error('Wrong operation!');
+        throw new Error('Wrong operation!');
     }
     if(typeof num1 !== "number" || typeof num2 !== "number"){
-        throw Error('Argument must be a number')
+        throw new Error('Argument must be a number')
     }
 
-    if( num2 === 0 && operation === '/'){
-        throw Error('Zero division operation is not possible')
-    }
     switch(operation){
         case '+':
             return num1 + num2;
@@ -18,11 +16,15 @@ function calculator(num1, num2, operation) {
             return num1 - num2;
             break;
         case '/':
+            if( num2 === 0){
+                throw Error('Zero division operation is not possible');
+            }
             return num1 / num2;
             break;
         case '*':
             return num1 * num2;
     }
+   
    }
 export{
     calculator
--- test/test_negative.js
+++ test/test_negative.js
@@ -4,16 +4,16 @@ import {calculator} from "../app.js"
 describe("Fields of negative tests for calculator function", function() {
         it("Should return Error('Wrong operation!') when function doesn't match correct operation", ()=>{
                 assert.throws(()=> {calculator(3,3,'%')}, Error)
-                assert.throws(()=> {calculator(3,5,'!')}, Error)
+                expect(()=> {calculator(3,5,'!')}).to.throw('Wrong operation!');
         });
         it("Should return Error('Argument must be a number') when first or second argument is not a number", ()=>{
                 assert.throws(() => {calculator('1',1, '+'), Error} )
-                assert.throws(() => {calculator(1,'1', '-'), Error} )
+                expect(()=> calculator('1',1, '+')).to.throw('Argument must be a number');
         });
        
         it("Should return Error('Zero division operation is not possible') when divide number to 0", ()=>{
-              assert.throws(()=>{calculator(5, 0, '/')}, Error)
               assert.throws(()=>{calculator(0, 0, '/')}, Error)
+              expect(()=> calculator(0, 0, '/')).to.throw('Zero division operation is not possible');
         });
         it('Check incorect values',()=>{
                 expect(calculator(0,0,'*')).to.not.eq(1);
